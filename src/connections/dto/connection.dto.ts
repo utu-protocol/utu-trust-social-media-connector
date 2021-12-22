41b22ef7dca0a54ce4511b3265599246815bb397
musayann <musayann@octangrp.com>
2021-12-22 10:10:12 +0200

    chore: add swagger api

--- src/connections/dto/connection.dto.ts
+++ src/connections/dto/connection.dto.ts
@@ -1,14 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ConnectionDto {
+  @ApiProperty({
+    description: 'Ethereum style address',
+  })
   @IsNotEmpty()
   @IsString()
   address: string;
 
+  @ApiProperty({
+    description: 'UTU Trust Client id',
+  })
   @IsNotEmpty()
   @IsString()
   clientId: string;
 
+  @ApiProperty({
+    description: 'Social connection token',
+  })
   @IsNotEmpty()
   @IsString()
   token: string;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('UTU Trust Social Media Connector')
+    .setDescription(
+      'Service for verifying social media connections and connecting social media data to the utu api',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
