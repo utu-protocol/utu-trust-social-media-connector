36b531553f222f8a875afdbe7f8ee9baca00fa7d
Raphael Karanja <raphael@utu.io>
2021-12-23 13:51:49 +0300

    feat: connect twitter

--- src/logins/dto/twitter-login.dto.ts
+++ src/logins/dto/twitter-login.dto.ts
@@ -1,1 +1,15 @@
-export class TwitterLoginDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class TwitterLoginDto {
+    @IsNotEmpty()
+    @IsString()
+    oauth_token: string;
+
+    @IsNotEmpty()
+    @IsString()
+    oauth_verifier: string;
+
+    @IsNotEmpty()
+    @IsString()
+    oauth_token_secret: string;
+}
--- src/logins/logins.controller.ts
+++ src/logins/logins.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { LoginsService } from './logins.service';
-import { TwitterLoginDto } from './dto/twitter-login.dto';
+import {Controller, Post, Body} from '@nestjs/common';
+import {LoginsService} from './logins.service';
+import {TwitterLoginDto} from './dto/twitter-login.dto';
 
 @Controller('logins')
 export class LoginsController {
-  constructor(private readonly loginsService: LoginsService) {}
+    constructor(private readonly loginsService: LoginsService) {
+    }
 
-  @Post('twitter')
-  create(@Body() twitterLoginDto: TwitterLoginDto) {
-    return this.loginsService.twitter(twitterLoginDto);
-  }
+    @Post('twitter/oauth/request_token')
+    create() {
+        return this.loginsService.twitterRequestToken();
+    }
+
+    @Post('twitter/oauth/access_token')
+    createAccessToken(@Body() twitterLoginDto: TwitterLoginDto) {
+        return this.loginsService.twitterAccessToken(twitterLoginDto);
+    }
 }
