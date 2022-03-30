ab5f1f776e16f40567405b92643f8e62137fd3f9
Bastian <bastian@maramoja.co.ke>
2022-03-30 14:35:40 +0300

    #CORE-333: fix sending of "bidirectional" and "properties" properties for POST /relationship

--- src/connections/twitter-relations.processor.ts
+++ src/connections/twitter-relations.processor.ts
@@ -53,12 +53,12 @@ export class TwitterRelationConsumer {
             uuid: address,
             address: address,
             twitter: id,
-          },
-          bidirectional: false,
-          properties: {
-            kind: 'twitter',
-          },
+          }
         },
+        bidirectional: false,
+        properties: {
+          kind: 'twitter',
+        }
       };
     });
     await this.sendRequests(relations, clientId);
@@ -87,12 +87,12 @@ export class TwitterRelationConsumer {
           type: 'Address',
           ids: {
             twitter: follower.id,
-          },
-          bidirectional: false,
-          properties: {
-            kind: 'twitter',
-          },
+          }
         },
+        bidirectional: false,
+        properties: {
+          kind: 'twitter',
+        }
       };
     });
     await this.sendRequests(relations, clientId);
